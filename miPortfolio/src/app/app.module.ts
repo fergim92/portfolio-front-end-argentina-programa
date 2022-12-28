8ec00573c03b08990b66812150f355cb05644b21
Fernando Gimenez <gimenezfernandomarcos@gmail.com>
2022-12-28 09:55:36 -0300

    Barras de progreso de habilidades

--- miPortfolio/src/app/app.module.ts
+++ miPortfolio/src/app/app.module.ts
@@ -10,7 +10,8 @@ import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
-
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
 
 
 @NgModule({
@@ -23,11 +24,13 @@ import { EducacionComponent } from './components/educacion/educacion.component';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
+    HysComponent,
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
