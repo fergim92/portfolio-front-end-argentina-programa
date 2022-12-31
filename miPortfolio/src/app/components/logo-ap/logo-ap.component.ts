7af1c5e65fbc1aaf44e9604666b32bfd61bef57d
Fernando Gimenez <gimenezfernandomarcos@gmail.com>
2022-12-30 21:22:52 -0300

    Ultimos cambios

--- miPortfolio/src/app/components/logo-ap/logo-ap.component.ts
+++ miPortfolio/src/app/components/logo-ap/logo-ap.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-logo-ap',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./logo-ap.component.css']
 })
 export class LogoAPComponent {
-
+  constructor(private router:Router){}
+  login(){
+    this.router.navigate(['/login'])
+  }
 }
+
+  
